5f09742937d82a7853c6d698e64e2027454a1463
Fiderana-17 <hei.fiderana@gmail.com>
2025-09-03 09:12:54 +0300

    feat: GET /health qui retourne le texte brute Ok

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 from fastapi import FastAPI
 
+
 app = FastAPI()
 
-# Q1 : GET /hello
-@app.get("/hello")
-def hello():
-    return {"message": "Hello world"}
-
-# Q2 : GET /welcome?name=xxx
-@app.get("/welcome")
-def welcome(name: str):
-    return {"message": f"Welcome {name}"}
+# a. GET /health
+@app.get("/health")
+def health():
+    return "Ok"
